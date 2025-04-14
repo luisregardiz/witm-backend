dda682f5e1c355e78c5b0b91f585057a4ac44f3e
luisregardiz <gadeeks@gmail.com>
2025-04-14 15:30:42 -0400

    feat: Update server configuration to enable CORS and change listening port to 4000; add VSCode settings file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS
+  app.enableCors({
+    origin: true, // You can replace this with your frontend URL in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(4000);
 }
 bootstrap();
